86847ebe437d45b3b7cead5eddb64ca480d7950b
Emmanuel Béziat <contact@emmanuelbeziat.com>
2016-06-09 02:36:41 +0200

    Tweets component

---
+++ src/components/app-tweets.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="commits">
+		<h2>Mes derniers tweets</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+		}
+	}
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,14 @@
 <template>
 	<section>
 		<h1>Home</h1>
+		<app-tweets></app-tweets>
 		<app-commits></app-commits>
 	</section>
 </template>
 
 <script>
 import appCommits from 'components/app-commits'
+import appTweets from 'components/app-tweets'
 
 export default {
 	data () {
@@ -15,7 +17,8 @@ export default {
 	},
 
 	components: {
-		appCommits
+		appCommits,
+		appTweets
 	}
 }
 
