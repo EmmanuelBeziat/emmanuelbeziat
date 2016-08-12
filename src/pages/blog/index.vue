73c767b161d9da98ae149b126329bd5acc1ded87
Emmanuel Béziat <contact@emmanuelbeziat.com>
2016-08-12 21:01:47 +0200

    Posts page

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,8 +1,13 @@
 <template>
-	<section class="posts">
-		<article class="article" v-for="post in posts">
-			<h1 class="article__title"><a v-link="{ name: 'blog-post', params: { slug: post.basename } }">{{ post.title }}</a></h1>
-			<div class="article__date">Posté le {{ post.date | moment 'dddd Do MMM YYYY[, à] HH[h]mm' }}</div>
+	<section class="posts-list">
+		<article class="article posts-list__item" v-for="post in posts">
+			<h1 class="article__title"><a v-link="{ name: 'blog-post', params: { slug: post.basename }}">{{ post.title }}</a></h1>
+			<div class="article__infos">
+				<div class="article__date">Posté le <time>{{ post.date | moment 'dddd Do MMM YYYY[, à] HH[h]mm' }}</time></div>
+				<div class="article__tags">
+					<span class="article__tag" v-for="tag in post.tags">{{ tag }}</span>
+				</div>
+			</div>
 		</article>
 	</section>
 </template>
--- src/pages/blog/post.vue
+++ src/pages/blog/post.vue
@@ -1,9 +1,13 @@
 <template>
 	<article class="article">
-		<h1>Toucheur de verges</h1>
 		<header class="article__header">
 			<h1 class="article__title">{{ post.title }}</h1>
-			<div class="article__date">{{ post.date | moment 'dddd Do MMM YYYY[, à] HH[h]mm' }}</div>
+			<div class="article__infos">
+				<div class="article__date">Posté le <time>{{ post.date | moment 'dddd Do MMM YYYY[, à] HH[h]mm' }}</time></div>
+				<div class="article__tags">
+					<span class="article__tag" v-for="tag in post.tags">{{ tag }}</span>
+				</div>
+			</div>
 		</header>
 		<div class="article__content" v-linkable>{{{ post.content }}}</div>
 	</article>
