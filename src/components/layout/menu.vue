6c86aae9d7b66125ddd37ee0d5e06b41ea0a2adc
Emmanuel Béziat <emmanuel@italic.fr>
2016-12-15 17:57:27 +0100

    menu

--- src/components/layout/menu.vue
+++ src/components/layout/menu.vue
@@ -1,6 +1,5 @@
 <template>
 	<nav class="site-menu">
-		<button class="site-menu__button" @click="menuMobile"><span class="site-menu__icon" aria-hidden="true"><span></span></span> Menu</button>
 		<a class="site-menu__link" v-link="{ name: 'home', exact: true }">Accueil</a>
 		<a class="site-menu__link" v-link="{ name: 'portfolio' }">Portfolio</a>
 		<a class="site-menu__link" v-link="{ name: 'blog' }">Blog</a>
