0610c94ba8404f25d212d04c0b06dd4a64a0c3d5
Emmanuel Béziat <emmanuel@italic.fr>
2016-12-22 04:10:31 +0100

    setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,39 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module'
 	},
-	// https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-	extends: 'standard',
-	// required to lint *.vue files
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
 	plugins: [
 		'html'
 	],
-	'env': {
-		'es6': true,
-		'browser': true
-	},
-	// add your custom rules here
-	'rules': {
-		'indent': ['error', 'tab'],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
 		// allow paren-less arrow functions
 		'arrow-parens': 0,
 		// allow async-await
 		'generator-star-spacing': 0,
 		// allow debugger during development
 		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-		// No semilocons
-		'semi': ['error', 'never' ],
-		// Single quotes
-		'quotes': ['error', 'single']
-	}
-}
+    }
+};
--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
@@ -27,6 +27,6 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   }
 }
