f2825d7b0bf6dccc6ff4cabb3b5e6d54ed0f3fd0
Emmanuel Béziat <emmanuel@italic.fr>
2016-12-22 03:54:07 +0100

    linit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,31 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	// https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+	extends: 'standard',
+	// required to lint *.vue files
+	plugins: [
+		'html'
+	],
+	'env': {
+		'es6': true,
+		'browser': true
+	},
+	// add your custom rules here
+	'rules': {
+		'indent': ['error', 'tab'],
+		// allow paren-less arrow functions
+		'arrow-parens': 0,
+		// allow async-await
+		'generator-star-spacing': 0,
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+		// No semilocons
+		'semi': ['error', 'never' ],
+		// Single quotes
+		'quotes': ['error', 'single']
+	}
 }
