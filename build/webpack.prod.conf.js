d724e3e8af1a22d2c0645e50a03c9a5d0e69b53c
EmmanuelBeziat <contact@emmanuelbeziat.com>
2017-01-23 12:13:58 +0100

    better js prod

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -38,7 +38,8 @@ var webpackConfig = merge(baseWebpackConfig, {
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
-            }
+            },
+            comments: false
         }),
         new webpack.optimize.OccurenceOrderPlugin(),
         // extract css into its own file
