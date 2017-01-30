60191d1d49172d95c51b8c4fa4c5644a8e663a80
EmmanuelBeziat <contact@emmanuelbeziat.com>
2017-01-30 04:42:45 +0100

    loader + plugins

---
+++ loader/post-loader.js
@@ -0,0 +1,25 @@
+var markdown = require('markdown-parse')
+var metaData = {}
+var rawContent
+
+module.exports = function (post) {
+
+	markdown(post, function(err, result) {
+		rawContent = result.body
+		metaData.title = result.attributes.title
+		metaData.image = result.attributes.image || 'https://images.emmanuelbeziat.com/social-default-fb.jpg'
+		metaData.date = result.attributes.date || new Date()
+		metaData.tags = result.attributes.tags || ['']
+		metaData.clients = result.attributes.clients || ['']
+		metaData.categories = result.attributes.categories || ['non-classe']
+		metaData.template = result.attributes.template || 'post'
+		metaData.description = result.attributes.description || ''
+		metaData.disqus = result.attributes.disqus || true
+		metaData.publish = result.attributes.publish || true
+	});
+
+	return JSON.stringify({
+		rawContent: rawContent,
+		metaData: metaData
+	})
+}
