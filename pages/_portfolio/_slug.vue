181b65cb8a761a37aef9ca69f4f0efe2dd6d11a8
EmmanuelBeziat <contact@emmanuelbeziat.com>
2017-03-19 02:58:20 +0100

    portfolio

---
+++ pages/_portfolio/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+	<article class="post">
+		<h1>{{ title }}</h1>
+		{{ body }}
+	</article>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	validate ({ params }) {
+		return !isNaN(+params.slug)
+	},
+
+	data ({ params, error }) {
+		return axios.get(`http://localhost:3001/portfolio/${+params.slug}`)
+		.then((res) => res.data)
+		.catch(() => {
+			error({ message: 'Post introuvable', statusCode: 404 })
+		})
+	},
+
+	transition (to, from) {
+		if (!from) return 'slide-left'
+		return +to.query.page < +from.query.page ? 'slide-right' : 'slide-left'
+	}
+}
+</script>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,11 +1,38 @@
 <template>
 	<section class="portfolio">
-		<h1>Portfolio</h1>
+		<div class="portfolio__list">
+			<router-link v-for="post in posts" class="portfolio__item" :to="'/portfolio/'+post.slug" >
+				<div class="portfolio__layer" :class="post.color">
+					<svg class="portfolio__image icon" :v-svg="post.image"></svg>
+				</div>
+
+				<div class="portfolio__caption">
+					<h2 class="portfolio__title">{{ post.title }}</h2>
+
+					<div class="portfolio__see-more">Jeter un oeil</div>
+				</div>
+			</router-link>
+		</div>
+
+		<div class="c-note c-note--success">
+			<div class="portfolio__thanks">
+				Et bien d’autres au fil des ans <small>(merci, hé ! <i class="icon-heart"></i>)</small>
+			</div>
+		</div>
 	</section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+	data () {
+		return axios.get('http://localhost:3001/portfolio')
+		.then((res) => {
+			return { posts: res.data }
+		})
+	},
+
 	transition (to, from) {
 		if (!from) return 'slide-left'
 		return +to.query.page < +from.query.page ? 'slide-right' : 'slide-left'
