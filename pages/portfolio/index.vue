f5063244d0f48302a174df096be8f1a179b7105b
Emmanuel Béziat <contact@emmanuelbeziat.com>
2018-05-12 11:26:22 +0200

    refine

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,19 +1,19 @@
 <template>
 	<section class="portfolio">
-		<div class="portfolio__list">
-			<router-link v-for="ref in refs" :key="ref.slug" class="portfolio__item" :to="'/portfolio/'+ref.slug" >
-				<div :class="['portfolio__layer', ref.color]">
-					<svg viewBox="0 0 100 100" class="portfolio__image icon">
+		<div class="showcase__list">
+			<router-link v-for="ref in refs" :key="ref.slug" class="showcase__item" :to="'/portfolio/'+ref.slug" >
+				<div :class="['showcase__layer', ref.color]">
+					<svg viewBox="0 0 100 100" class="showcase__image icon">
 						<use :xlink:href="'/sprite.svg#' + ref.image"></use>
 					</svg>
 				</div>
 
-				<div class="portfolio__caption">
-					<h2 class="portfolio__title">{{ ref.title }}</h2>
+				<div class="showcase__caption">
+					<h2 class="showcase__title">{{ ref.title }}</h2>
 					<div class="portfolio-tags">
 						<i class="icon-tag" aria-hidde="true"></i>
-						<div class="portfolio-tags__list">
-							<span class="portfolio-tags__item" v-for="tag in ref.tags" :key="tag">{{ tag }}</span>
+						<div class="showcase-tags__list">
+							<span class="showcase-tags__item" v-for="tag in ref.tags" :key="tag">{{ tag }}</span>
 						</div>
 					</div>
 				</div>
@@ -21,7 +21,7 @@
 		</div>
 
 		<div class="c-note c-note--success">
-			<div class="portfolio__thanks">
+			<div class="showcase__thanks">
 				Et bien d’autres au fil des ans <small>(merci, hé ! <i class="icon-heart"></i>)</small>
 			</div>
 		</div>
@@ -55,114 +55,6 @@ export default {
 <style lang="stylus">
 @require '~assets/styles/variables.styl'
 @require '~assets/styles/mixins.styl'
-
-.portfolio__list
-	display grid
-	grid-template-columns 1fr 1fr 1fr
-	grid-column-gap 4px
-	grid-row-gap 4px
-
-	@media (max-width $breakpoint-tablet)
-		grid-template-columns 1fr 1fr
-
-	@media (max-width $breakpoint-mobile)
-		grid-template-columns 1fr
-
-.portfolio__item
-	position relative
-	color $color-text
-
-.portfolio__layer
-	background $color-separator
-	position relative
-	height rem(240px)
-	display flex
-	justify-content center
-	align-items center
-	z-index 10
-	transform-origin 50% 20px
-	transition .2s ease-in-out
-	backface-visibility hidden
-
-.portfolio__image
-	size rem(128px)
-	fill $color-text
-	backface-visibility hidden
-	transition .2s ease-in-out .1s
-
-.portfolio__caption
-	z-index 5
-	position absolute
-	top 0
-	right 0
-	bottom 0
-	left 0
-	padding rem(20px)
-	background darken($color-background, 10%)
-	display flex
-	flex-direction column
-	justify-content flex-end
-	overflow hidden
-	text-align center
-
-.portfolio__title
-	margin 0 0 .5em
-	line-height 1.5
-	font-size rem(28px)
-	font-weight 400
-
-.portfolio__item:hover
-	.portfolio__layer
-		transform scale(.4) translateZ(0)
-		border-radius 50%
-		size rem(240px)
-		margin auto
-
-		&.blue
-			background $color-blue
-
-		&.red
-			background $color-red
-
-		&.violet
-			background $color-violet
-
-		&.yellow
-			background darken($color-yellow, 10%)
-
-		&.green
-			background $color-green
-
-		&.black
-			background darken($color-grey, 50%)
-
-	.portfolio__image
-		transform scale(1.1) translateZ(0)
-
-.portfolio-tags
-	display flex
-	align-items center
-	justify-content center
-
-	i
-		margin-right rem(10px)
-
-.portfolio-tags__list
-	text-align left
-	line-height 1.15
-
-.portfolio-tags__item
-	font-size rem(14px)
-	color $color-text
-
-	&:not(:last-child)::after
-		content ", "
-
-.portfolio__thanks
-	font-size $font-size-heading-4
-
-	.icon-heart
-		color $color-red
-
+@require '~assets/styles/components/showcase.styl'
 @require '~assets/styles/modules/notes.styl'
 </style>
--- pages/projets/index.vue
+++ pages/projets/index.vue
@@ -1,21 +1,23 @@
 <template>
-	<section class="blog">
+	<section class="projects">
 		<div class="c-search">
 			<input class="c-search__input" type="search" placeholder="Recherche…" v-model="search">
 		</div>
 
-		<div class="projet-list">
-			<article class="projet-list__item" v-for="projet in filteredList" :key="projet.id">
-				<h1 class="projet-list__title"><a :href="projet.html_url">{{ projet.name }}</a></h1>
-				<div class="projet-list__infos">
-					<div class="projet-list__date">Créé le <time>{{ projet.created_at | moment('Do MMMM YYYY') }}</time> — Dernière mise à jour le <time>{{ projet.updated_at | moment('Do MMMM YYYY') }}</time></div>
+		<div class="post-list">
+			<article class="post-list__item" v-for="projet in filteredList" :key="projet.id">
+				<h1 class="post-list__title"><a :href="projet.html_url">{{ projet.name }}</a></h1>
+
+				<div class="post-list__infos">
+					<div class="post-list__date">Créé le <time>{{ projet.created_at | moment('Do MMMM YYYY') }}</time> — Dernière mise à jour le <time>{{ projet.updated_at | moment('Do MMMM YYYY') }}</time></div>
 
 					<div class="post-list__tags">
 						<span class="c-tag">{{ projet.language }}</span>
 						<span class="c-tag"><i class="icon-star" aria-hidden="true"></i> {{ projet.stargazers_count }}</span>
 					</div>
 				</div>
-				<div class="projet-list__description">{{ projet.description }}</div>
+
+				<div class="post-list__description">{{ projet.description }}</div>
 			</article>
 		</div>
 	</section>
@@ -59,60 +61,7 @@ export default {
 <style lang="stylus">
 @require '~assets/styles/variables.styl'
 @require '~assets/styles/mixins.styl'
-
-.projet-list__item + .projet-list__item
-	margin-top 2.5em
-	padding-top 2.5em
-	border-top 1px solid $color-separator
-
-.projet-list__title
-	margin 0
-	font 400 $font-size-article-title-small/1.5 $font-stack-heading
-
-	@media (max-width $breakpoint-mobile)
-		line-height 1.25
-		overflow auto
-
-.projet-list__infos
-	font-size $font-size-article-infos
-	display flex
-	justify-content space-between
-	align-items center
-
-	@media (max-width $breakpoint-mobile)
-		flex-direction column
-		align-items flex-start
-
-.projet-list__date
-	@media (max-width $breakpoint-mobile)
-		margin .5em 0
-
-.projet-list__tags
-	max-width 50%
-
-	@media (max-width $breakpoint-mobile)
-		max-width none
-
-.projet-list__header
-	margin-bottom 1em
-
-.projet-list__content
-	margin-bottom 1em
-
-.projet-list__top-link
-	float right
-	background $color-green
-	margin-top rem(10px)
-	color $color-background
-	border-radius rem(2px)
-
-	&:hover
-		background $color-red
-		color $color-background
-
-	@media (min-width $breakpoint-mobile)
-		display none
-
+@require '~assets/styles/components/posts.styl'
 @require '~assets/styles/modules/search.styl'
 @require '~assets/styles/modules/tags.styl'
 </style>
