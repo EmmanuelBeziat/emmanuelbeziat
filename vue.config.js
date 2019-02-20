071728b0eed76bb7607a54c819fb6f9a976b0afa
Emmanuel Béziat <contact@emmanuelbeziat.com>
2019-02-20 20:40:27 +0100

    re-enable

--- vue.config.js
+++ vue.config.js
@@ -15,12 +15,18 @@ module.exports = {
 
 	pwa: {
 		name: 'Emmanuel Béziat',
-		themeColor: '',
-		msTileColor: '',
-		appleMobileWebAppCapable: '',
-		appleMobileWebAppStatusBarStyle: '',
-		manifestPath: '',
-		iconPaths: {}
+		themeColor: '#233238',
+		msTileColor: '#233238',
+		appleMobileWebAppCapable: 'no',
+		appleMobileWebAppStatusBarStyle: 'white',
+		manifestPath: 'favicons/site.webmanifest',
+		iconPaths: {
+			favicon32: 'favicons/favicon-32x32.png',
+			favicon16: 'favicons/favicon-16x16.png',
+			appleTouchIcon: 'favicons/apple-touch-icon.png',
+			maskIcon: 'favicons/safari-pinned-tab.svg',
+			msTileImage: 'favicons/mstile-144x144.png'
+		}
 	},
 
 	outputDir: '../site',
