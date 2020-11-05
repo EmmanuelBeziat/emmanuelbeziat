b001470994cbb983287b2896b0a1b7370a096cb1
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-11-05 22:28:37 +0100

    drawing

---
+++ src/components/home/Drawing.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="drawing">
+		<img :src="require('@/assets/img/emmanuelb-moon.png')" alt="Emmanuel Béziat (Art by Marie-Laure Rouzier)" loading="lazy">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Drawing'
+}
+</script>
+
+<style lang="stylus" scoped>
+.drawing
+	text-align center
+
+	img
+		vertical-align top
+		max-width 100%
+</style>
