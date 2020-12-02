777ac8ea5b135c99dc5efb513229d82cdd416f93
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-12-03 00:19:44 +0100

    new loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,9 +1,15 @@
 <template>
 	<div class="loader">
-		<svg width="80" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-			<path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-				<animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
-			</path>
+		<svg width="100" x="50%" y="0px" viewBox="0 0 100 100">
+			<circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+				<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"></animate>
+			</circle>
+			<circle fill="#fff" stroke="none" cx="26" cy="50" r="6">
+				<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2"></animate>
+			</circle>
+			<circle fill="#fff" stroke="none" cx="46" cy="50" r="6">
+				<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3"></animate>
+			</circle>
 		</svg>
 	</div>
 </template>
