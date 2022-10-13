9712a2d51c7dfae10b3910ca30c46b0884b8f849
Emmanuel Béziat <contact@emmanuelbeziat.com>
2022-10-13 03:42:09 +0200

    test nvm bash

--- src/main.js
+++ src/main.js
@@ -16,4 +16,4 @@ app.use(sequentialEntrance)
 app.mount('#app')
 
 /* eslint-disable no-debugger, no-console */
-console.log('%c Made with 🕑 & 💖 by Emmanuel Béziat', 'background: #c3e88d; color: #233238; padding: .5em 1em;')
+console.log('%c Made with 🕑 and 💖 by Emmanuel Béziat', 'background: #c3e88d; color: #233238; padding: .5em 1em;')
