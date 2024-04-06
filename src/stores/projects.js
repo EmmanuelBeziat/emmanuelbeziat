ee42c6afdf1404238f0e4441bd4dd7c0d024c8e8
Emmanuel Béziat <contact@emmanuelbeziat.com>
2024-04-06 05:48:01 +0200

    update search

--- src/stores/projects.js
+++ src/stores/projects.js
@@ -16,29 +16,31 @@ export const useProjectsStore = defineStore({
 
 	actions: {
 		async fetch () {
-			await fetch(api.projects)
+			await fetch(`${api.projects}?sort=update`)
 				.then(response => response.json())
 				.then(data => {
-					this.projects = data.map(repo => {
-						return {
-							id: repo.id,
-							name: repo.name,
-							url: repo.html_url,
-							homepage: repo.homepage,
-							description: repo.description,
-							created_at: repo.created_at,
-							updated_at: repo.updated_at,
-							pushed_at: repo.pushed_at,
-							language: repo.language,
-							languages_url: repo.languages_url,
-							stargazers: repo.stargazers_count,
-							watchers: repo.watchers_count,
-							license: repo.license ? repo.license.key : null,
-							forks: repo.forks_count,
-							issues: repo.open_issues_count,
-							archived: repo.archived
-						}
-					}).sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at))
+					this.projects = data
+						.filter(repo => !repo.archived)
+						.map(repo => {
+							return {
+								id: repo.id,
+								name: repo.name,
+								url: repo.html_url,
+								homepage: repo.homepage,
+								description: repo.description,
+								created_at: repo.created_at,
+								updated_at: repo.updated_at,
+								pushed_at: repo.pushed_at,
+								language: repo.language,
+								languages_url: repo.languages_url,
+								stargazers: repo.stargazers_count,
+								watchers: repo.watchers_count,
+								license: repo.license ? repo.license.key : null,
+								forks: repo.forks_count,
+								issues: repo.open_issues_count,
+								archived: repo.archived
+							}
+						}).sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at))
 				})
 				.catch(error => console.error(`Store error: ${error}`))
 		},
