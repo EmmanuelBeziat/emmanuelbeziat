7af8fed3475e1b57a6ca75400b64185d6a64da6b
Emmanuel Béziat <social@emmanuelbeziat.com>
2025-01-28 01:05:34 +0100

    feat: :lipstick: New favicon

--- vite.config.js
+++ vite.config.js
@@ -28,12 +28,12 @@ const pwa = {
 		background_color: '#1f2d32',
 		icons: [
 			{
-				src: '/favicons/android-chrome-192x192.png',
+				src: '/favicons/web-app-manifest-192x192.png',
 				sizes: '192x192',
 				type: 'image/png'
 			},
 			{
-				src: '/favicons/android-chrome-512x512.png',
+				src: '/favicons/web-app-manifest-512x512.png',
 				sizes: '512x512',
 				type: 'image/png'
 			}
