969ae8b231ce5b527724f91aaa2e2a0177ad53bd
Emmanuel Béziat <social@emmanuelbeziat.com>
2025-03-02 05:53:10 +0100

    fix: :bug: debug setup script

--- setup.js
+++ setup.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 async function createEnvFile() {
+	const envPath = path.join(__dirname, '.env')
 	if (fs.existsSync(envPath)) {
     console.log('.env file already exists. Skipping creation.')
     return
