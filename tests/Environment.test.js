9dd3bb9cd3e8c16fe3eb49b1a2dd22fbc7f0ff40
Emmanuel Béziat <social@emmanuelbeziat.com>
2025-03-31 03:19:56 +0200

    test: :green_heart: tests update

--- tests/Environment.test.js
+++ tests/Environment.test.js
@@ -3,7 +3,6 @@ import { describe, it, expect } from 'vitest'
 describe('Environment Variables', () => {
   it('should have PORT defined', () => {
     expect(process.env.VITE_PORT).toBeDefined()
-    expect(process.env.VITE_PORT).toBe('3033')
   })
 
   it('should have API_POSTS defined', () => {
@@ -20,26 +19,21 @@ describe('Environment Variables', () => {
 
   it('should have API_PROJECTS defined', () => {
     expect(process.env.VITE_API_PROJECTS).toBeDefined()
-    expect(process.env.VITE_API_PROJECTS).toBe('https://api.github.com/users/emmanuelbeziat/repos')
   })
 
   it('should have OG_TITLE defined', () => {
     expect(process.env.VITE_OG_TITLE).toBeDefined()
-    expect(process.env.VITE_OG_TITLE).toBe('Emmanuel Béziat')
   })
 
   it('should have OG_URL defined', () => {
     expect(process.env.VITE_OG_URL).toBeDefined()
-    expect(process.env.VITE_OG_URL).toBe('https://www.emmanuelbeziat.com')
   })
 
   it('should have OG_IMAGE defined', () => {
     expect(process.env.VITE_OG_IMAGE).toBeDefined()
-    expect(process.env.VITE_OG_IMAGE).toBe('/branding/opengraph.jpg')
   })
 
   it('should have OG_DESCRIPTION defined', () => {
     expect(process.env.VITE_OG_DESCRIPTION).toBeDefined()
-    expect(process.env.VITE_OG_DESCRIPTION).toBe('Portfolio en ligne d’un développeur web du sud. Billets de blogs, tutoriels, astuces, diatribes et réflexions sur le métier, le code et plein d’autres choses.')
   })
 })
