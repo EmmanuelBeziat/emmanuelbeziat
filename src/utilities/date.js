a9d4d32a324f449fa299c6db822d6b37c861aa2c
Emmanuel Béziat <social@emmanuelbeziat.com>
2025-06-17 16:26:56 +0200

    refactor: :art:

--- src/utilities/date.js
+++ src/utilities/date.js
@@ -1,8 +1,7 @@
 /**
  * Formats a date value according to the specified locale and options.
- *
  * @param {string|number|Date} value - The date value to format. Can be a string, number, or Date object.
  * @param {Object} options - An object with options to customize the date format.
  * @returns {string} - The formatted date string.
  */
-export const dateFormat = (value, options) => new Intl.DateTimeFormat('fr-FR', options).format(new Date(value))
+export const dateFormat = (value, options = {}) => new Intl.DateTimeFormat('fr-FR', options).format(new Date(value))
